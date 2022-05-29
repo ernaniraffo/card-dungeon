195b8b25b31eb30920d97b66a37394c610117963
Ernani Raffo <eraffo@ucsc.edu>
2022-05-28 21:29:26 -0700

    restarting levels

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ let config = {
     width: 800,
     height: 400,
     backgroundColor: 0x00000000,
-    scene: [Menu, Play, GameOver, GameOver2, Level2, CardSelect],
+    scene: [ Menu, Play, GameOver, GameOver2, Level2, CardSelect ],
     physics: {
         default: 'arcade',
         arcade: {
@@ -20,6 +20,32 @@ let enemyTurn = false;
 // 2D Array of Card Names, Stats + etc...
 // Name, Damage, Burn, Bleed, Strength
 let cardTypes = [
+    ["Ace", 1, 0, 0, 1],
+    ["Slash", 6, 0, 0, 0],
+    ["Reklass Charge", 4, 0, 0, 2],
+    ["Field Gauze", 0, 0, 0, 0],
+    ["Mangle", 6, 0, 3, 0, 0],
+    ["Stitchasone", 0, 0, 0, 3],
+    ["Flatten", 2, 0, 0, 0],
+    ["Blue Ace", 2, 0, 0, 2],
+    ["Strong Stance", 0, 0, 0, 0],
+    ["Parry", 0, 0, 0, 0],
+    ["Fan the Flames", 0, 2, 0, 0],
+    ["Fuzzy Friend", 0, 0, 0, 0],
+    ["Flying Fury Fist", 5, 0, 0, 0],
+    ["Flagellate", 0, 0, 3, 2],
+    ["Eldritch Lore", 0, 0, 0, 0],
+    ["Bludgeon", 5, 0, 0, 0],
+    ["Myst. Mixture", [0, 10], 0, 0, 0],
+    ["Ichor Bomb", [3, 4], 0, 0, 0],
+    ["Point Blank", [5, 7], 0, 0, 0],
+    ["Rot Myst", 0, 0, 0, 0],
+    ["Flesh Wound", 0, 0, 0, 0],
+    ["Fatal Blow", 0, 0, 0, 0],
+    ["Bile Bomb", [1, 5], 0, 0, 0]
+];
+
+let copyCardTypes = [
     ["Ace", 1, 0, 0, 1],
     ["Slash", 6, 0, 0, 0],
     ["Reklass Charge", 4, 0, 0, 2],
