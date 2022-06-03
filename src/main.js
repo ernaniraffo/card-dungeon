ee33fcfa67afbc7a533e667b699739619a18f810
Ernani Raffo <eraffo@ucsc.edu>
2022-06-03 16:09:15 -0700

    ace values

--- src/main.js
+++ src/main.js
@@ -21,14 +21,14 @@ let enemyTurn = false;
 // 2D Array of Card Names, Stats + etc...
 // Name, Damage, Burn, Bleed, Strength
 let cardTypes = [
-    ["Ace", 5, 0, 0, 10],
+    ["Ace", 1, 0, 0, 3],
     ["Slash", 6, 0, 0, 0],
     ["Reklass Charge", 4, 0, 0, 2],
     ["Field Gauze", 0, 0, 0, 0],
     ["Mangle", 6, 0, 3, 0, 0],
     ["Stitchasone", 0, 0, 0, 3],
     ["Flatten", 2, 0, 0, 0],
-    ["Blue Ace", 8, 0, 0, 15],
+    ["Blue Ace", 1, 0, 0, 5],
     ["Strong Stance", 0, 0, 0, 0],
     ["Parry", 0, 0, 0, 0],
     ["Fan the Flames", 0, 2, 0, 0],
@@ -47,14 +47,14 @@ let cardTypes = [
 ];
 
 let copyCardTypes = [
-    ["Ace", 5, 0, 0, 10],
+    ["Ace", 1, 0, 0, 3],
     ["Slash", 6, 0, 0, 0],
     ["Reklass Charge", 4, 0, 0, 2],
     ["Field Gauze", 0, 0, 0, 0],
     ["Mangle", 6, 0, 3, 0, 0],
     ["Stitchasone", 0, 0, 0, 3],
     ["Flatten", 2, 0, 0, 0],
-    ["Blue Ace", 2, 0, 0, 15],
+    ["Blue Ace", 1, 0, 0, 5],
     ["Strong Stance", 0, 0, 0, 0],
     ["Parry", 0, 0, 0, 0],
     ["Fan the Flames", 0, 2, 0, 0],
@@ -75,7 +75,7 @@ let copyCardTypes = [
 
 //starting deck
 let StartingDeck = [
-    ["Ace", 5, 0, 0, 10],
+    ["Ace", 1, 0, 0, 3],
     ["Slash", 6, 0, 0, 0],
     ["Reklass Charge", 4, 0, 0, 2],
     ["Field Gauze", 0, 0, 0, 0],
@@ -84,12 +84,12 @@ let StartingDeck = [
 
 //starting deck
 let StartingDeckCopy = [
-    ["Ace", 5, 0, 0, 10],
+    ["Ace", 1, 0, 0, 3],
     ["Slash", 6, 0, 0, 0],
     ["Reklass Charge", 4, 0, 0, 2],
     ["Field Gauze", 0, 0, 0, 0],
     ["Mangle", 6, 0, 3, 0, 0]
-] 
+]
 
 // global player strength + health
 let playerHealth;
